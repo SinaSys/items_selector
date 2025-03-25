83f6630353842f3ea1d0fa5b716440195ff1cf13
SINASYS <sinasystem47@gmail.com>
2025-03-25 17:42:19 +0430

    Add new extension for BaseSelectorState

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,17 +1,17 @@
 import 'package:items_selector/src/model/item_wrapper.dart';
 import 'package:items_selector/src/model/base_selector.dart';
+import 'package:items_selector/src/model/base_list_item_selector.dart';
 import 'package:items_selector/src/model/base_grid_item_selector.dart';
 import 'package:items_selector/src/widget/multi_grid_item_selector.dart';
 import 'package:items_selector/src/widget/multi_list_item_selector.dart';
 import 'package:items_selector/src/widget/single_grid_item_selector.dart';
-import 'package:items_selector/src/widget/single_list_item_selector.dart';
 
 extension BaseSelectorExtension<T> on BaseSelectorState<T> {
   BaseGridGridItemSelector get baseGridItemSelectorWidget => widget as BaseGridGridItemSelector<T>;
 
   SingleGridItemSelector get singleGridItemSelectorWidget => widget as SingleGridItemSelector<T>;
 
-  SingleListItemSelector get singleListItemSelectorWidget => widget as SingleListItemSelector<T>;
+  BaseListItemSelector get baseListItemSelectorWidget => widget as BaseListItemSelector<T>;
 
   MultiListItemSelector get multiListItemSelectorWidget => widget as MultiListItemSelector<T>;
 
