93e7256149477d3fb798704199e4307829091a29
SINASYS <sinasystem47@gmail.com>
2025-03-25 17:39:57 +0430

    Add direction property for SingleListItemSelector

--- lib/src/widget/single_list_item_selector.dart
+++ lib/src/widget/single_list_item_selector.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/material.dart';
-import 'package:items_selector/src/utils/extensions.dart';
 import 'package:items_selector/src/model/widget_type.dart';
-import 'package:items_selector/src/model/base_selector.dart';
-import 'package:items_selector/src/model/item_selector.dart';
 import 'package:items_selector/src/model/select_options.dart';
+import 'package:items_selector/src/model/base_list_item_selector.dart';
 
-class SingleListItemSelector<T> extends BaseSelector<T> {
+class SingleListItemSelector<T> extends BaseListItemSelector<T> {
   SingleListItemSelector({
     super.key,
     super.hasLongPress,
@@ -13,39 +10,10 @@ class SingleListItemSelector<T> extends BaseSelector<T> {
     required super.items,
     required super.selectedItems,
     required super.builder,
-    this.direction = Axis.horizontal,
     SingleSelectOptions? options,
+    super.direction,
   }) : super(
           options: options,
           type: WidgetType.singleListItemSelector,
         );
-
-  final Axis direction;
-
-  @override
-  SingleListItemSelectorState<T> createState() => SingleListItemSelectorState<T>();
-}
-
-class SingleListItemSelectorState<T> extends BaseSelectorState<T> {
-  Widget defineWidgetByDirection(List<Widget> items) {
-    return SingleChildScrollView(
-      scrollDirection: singleListItemSelectorWidget.direction,
-      child: switch (singleListItemSelectorWidget.direction) {
-        Axis.horizontal => Row(children: items),
-        Axis.vertical => Column(children: items)
-      },
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final List<Widget> listItem = List.generate(
-      itemsWrapper.length,
-      (index) {
-        ItemSelector item = widget.builder(context, index);
-        return itemContainer(itemsWrapper[index], index, item);
-      },
-    );
-    return defineWidgetByDirection(listItem);
-  }
 }
