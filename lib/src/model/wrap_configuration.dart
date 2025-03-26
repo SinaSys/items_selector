b4f7e3a84c7a72a999d6d529a07e2f522a3fdb06
SINASYS <sinasystem47@gmail.com>
2025-03-26 21:04:36 +0430

    Implement WrapConfiguration class

---
+++ lib/src/model/wrap_configuration.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/widgets.dart';
+
+class WrapConfiguration extends Wrap {
+  WrapConfiguration({
+    super.key,
+    super.alignment,
+    super.clipBehavior,
+    super.crossAxisAlignment,
+    super.direction,
+    super.runAlignment,
+    super.runSpacing,
+    super.spacing,
+    super.textDirection,
+    super.verticalDirection,
+  }) : super(children: []);
+}
