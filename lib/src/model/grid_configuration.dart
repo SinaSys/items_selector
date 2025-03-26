46bb240cfa76d007de63f66d2357e5efe9b5f27e
SINASYS <sinasystem47@gmail.com>
2025-03-26 21:06:48 +0430

    Refactor GridConfiguration class without defining fields individually

--- lib/src/model/grid_configuration.dart
+++ lib/src/model/grid_configuration.dart
@@ -1,46 +1,29 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter/gestures.dart' show DragStartBehavior;
 
-class GridConfiguration {
-  const GridConfiguration({
-    required this.gridDelegate,
-    this.reverse = false,
-    this.controller,
-    this.primary,
-    this.physics,
-    this.shrinkWrap = false,
-    this.padding,
-    this.addAutomaticKeepAlive = false,
+class GridConfiguration extends GridView {
+  GridConfiguration({
+    super.key,
+    super.scrollDirection,
+    super.reverse,
+    super.controller,
+    super.primary,
+    super.physics,
+    super.shrinkWrap,
+    super.padding,
+    required super.gridDelegate,
+    this.findChildIndexCallback,
     this.addRepaintBoundaries = true,
     this.addSemanticIndexes = true,
-    this.cacheExtent,
-    this.semanticChildCount,
-    this.dragStartBehavior = DragStartBehavior.start,
-    this.clipBehavior = Clip.hardEdge,
-    this.keyboardDismissBehavior = ScrollViewKeyboardDismissBehavior.manual,
-    this.restorationId,
-    this.hitTestBehavior = HitTestBehavior.opaque,
-    this.scrollDirection = Axis.vertical,
-    this.findChildIndexCallback,
+    super.cacheExtent,
+    super.dragStartBehavior,
+    super.keyboardDismissBehavior,
+    super.restorationId,
+    super.clipBehavior,
+    super.hitTestBehavior,
+    super.addAutomaticKeepAlives,
   });
 
-  final SliverGridDelegate gridDelegate;
-  final bool reverse;
-  final ScrollController? controller;
-  final bool? primary;
-  final ScrollPhysics? physics;
-  final bool shrinkWrap;
-  final EdgeInsetsGeometry? padding;
-  final bool addAutomaticKeepAlive;
   final bool addRepaintBoundaries;
   final bool addSemanticIndexes;
-  final double? cacheExtent;
-  final int? semanticChildCount;
-  final DragStartBehavior dragStartBehavior;
-  final Clip clipBehavior;
-  final ScrollViewKeyboardDismissBehavior keyboardDismissBehavior;
-  final String? restorationId;
-  final HitTestBehavior hitTestBehavior;
-  final Axis scrollDirection;
   final ChildIndexGetter? findChildIndexCallback;
 }
