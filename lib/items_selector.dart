eedc5eb867ed41717c2d0383735890ca28b9893d
SINASYS <sinasystem47@gmail.com>
2025-03-26 21:19:46 +0430

    Expose wrap-based selectors and WrapConfiguration in library

--- lib/items_selector.dart
+++ lib/items_selector.dart
@@ -3,8 +3,11 @@ library;
 export 'src/model/item_selector.dart';
 export 'src/model/select_options.dart';
 export 'src/model/grid_configuration.dart';
+export 'src/model/wrap_configuration.dart';
 
 export 'src/widget/multi_grid_item_selector.dart';
 export 'src/widget/multi_list_item_selector.dart';
 export 'src/widget/single_grid_item_selector.dart';
 export 'src/widget/single_list_item_selector.dart';
+export 'src/widget/single_wrap_item_selector.dart';
+export 'src/widget/multi_wrap_item_selector.dart';
