d24992a19fab8b2992d17ad509149d2b00bffdb0
SINASYS <sinasystem47@gmail.com>
2025-03-27 18:14:35 +0430

    Implement ListConfiguration

---
+++ lib/src/model/list_configuration.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ListConfiguration extends ListView {
+  ListConfiguration({
+    super.key,
+    super.addAutomaticKeepAlives,
+    super.addRepaintBoundaries,
+    super.addSemanticIndexes,
+    super.cacheExtent,
+    super.clipBehavior,
+    super.controller,
+    super.dragStartBehavior,
+    super.hitTestBehavior,
+    super.itemExtent,
+    super.padding,
+    super.physics,
+    super.primary,
+    super.keyboardDismissBehavior,
+    super.itemExtentBuilder,
+    super.prototypeItem,
+    super.restorationId,
+    super.reverse,
+    super.shrinkWrap,
+    super.semanticChildCount,
+  });
+}
