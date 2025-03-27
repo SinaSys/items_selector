a7530682343d2304f10caea4c237fd18e57e2e26
SINASYS <sinasystem47@gmail.com>
2025-03-28 00:00:35 +0430

    Improve BaseGridItemSelector readability

--- lib/src/model/base_grid_item_selector.dart
+++ lib/src/model/base_grid_item_selector.dart
@@ -25,36 +25,32 @@ class BaseGridGridItemSelector<T> extends BaseSelector<T> {
 class BaseGridGridItemSelectorState<T> extends BaseSelectorState<T> {
   @override
   Widget build(BuildContext context) {
-    final List<Widget> listItem = List.generate(
-      itemsWrapper.length,
-      (index) {
-        ItemSelector item = widget.builder(context, index);
-        return itemContainer(itemsWrapper[index], index, item);
-      },
-    );
+    final config = baseGridItemSelectorWidget.gridConfiguration;
+
     return GridView.builder(
-      findChildIndexCallback: baseGridItemSelectorWidget.gridConfiguration.findChildIndexCallback,
-      scrollDirection: baseGridItemSelectorWidget.gridConfiguration.scrollDirection,
-      hitTestBehavior: baseGridItemSelectorWidget.gridConfiguration.hitTestBehavior,
       key: baseGridItemSelectorWidget.key,
-      itemCount: listItem.length,
-      gridDelegate: baseGridItemSelectorWidget.gridConfiguration.gridDelegate,
-      shrinkWrap: baseGridItemSelectorWidget.gridConfiguration.shrinkWrap,
-      padding: baseGridItemSelectorWidget.gridConfiguration.padding,
-      controller: baseGridItemSelectorWidget.gridConfiguration.controller,
-      addRepaintBoundaries: baseGridItemSelectorWidget.gridConfiguration.addRepaintBoundaries,
-      addSemanticIndexes: baseGridItemSelectorWidget.gridConfiguration.addSemanticIndexes,
-      cacheExtent: baseGridItemSelectorWidget.gridConfiguration.cacheExtent,
-      clipBehavior: baseGridItemSelectorWidget.gridConfiguration.clipBehavior,
-      dragStartBehavior: baseGridItemSelectorWidget.gridConfiguration.dragStartBehavior,
-      keyboardDismissBehavior: baseGridItemSelectorWidget.gridConfiguration.keyboardDismissBehavior,
-      physics: baseGridItemSelectorWidget.gridConfiguration.physics,
-      primary: baseGridItemSelectorWidget.gridConfiguration.primary,
-      restorationId: baseGridItemSelectorWidget.gridConfiguration.restorationId,
-      reverse: baseGridItemSelectorWidget.gridConfiguration.reverse,
-      semanticChildCount: baseGridItemSelectorWidget.gridConfiguration.semanticChildCount,
-      itemBuilder: (BuildContext context, int index) {
-        return listItem[index];
+      findChildIndexCallback: config.findChildIndexCallback,
+      scrollDirection: config.scrollDirection,
+      hitTestBehavior: config.hitTestBehavior,
+      itemCount: itemsWrapper.length,
+      gridDelegate: config.gridDelegate,
+      shrinkWrap: config.shrinkWrap,
+      padding: config.padding,
+      controller: config.controller,
+      addRepaintBoundaries: config.addRepaintBoundaries,
+      addSemanticIndexes: config.addSemanticIndexes,
+      cacheExtent: config.cacheExtent,
+      clipBehavior: config.clipBehavior,
+      dragStartBehavior: config.dragStartBehavior,
+      keyboardDismissBehavior: config.keyboardDismissBehavior,
+      physics: config.physics,
+      primary: config.primary,
+      restorationId: config.restorationId,
+      reverse: config.reverse,
+      semanticChildCount: config.semanticChildCount,
+      itemBuilder: (context, index) {
+        final item = widget.builder(context, index);
+        return itemContainer(itemsWrapper[index], index, item);
       },
     );
   }
