831ea456902756d81094801f0a8297f4025a223c
SINASYS <sinasystem47@gmail.com>
2025-03-29 18:59:17 +0430

    Implement different type of list configuration

--- lib/src/model/grid_configuration.dart
+++ lib/src/model/grid_configuration.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/widgets.dart';
 
-class GridConfiguration extends GridView {
-  GridConfiguration({
+sealed class BaseGridConfiguration extends GridView {
+  BaseGridConfiguration({
     super.key,
     super.scrollDirection,
     super.reverse,
@@ -20,10 +20,113 @@ class GridConfiguration extends GridView {
     super.restorationId,
     super.clipBehavior,
     super.hitTestBehavior,
-    super.addAutomaticKeepAlives,
+    this.addAutomaticKeepAlives = false,
   });
 
   final bool addRepaintBoundaries;
   final bool addSemanticIndexes;
   final ChildIndexGetter? findChildIndexCallback;
+  final bool addAutomaticKeepAlives;
+}
+
+class BuilderConfiguration extends BaseGridConfiguration {
+  BuilderConfiguration({
+    super.key,
+    super.scrollDirection,
+    super.reverse,
+    super.controller,
+    super.primary,
+    super.physics,
+    super.shrinkWrap,
+    super.padding,
+    required super.gridDelegate,
+    super.findChildIndexCallback,
+    super.addRepaintBoundaries = true,
+    super.addSemanticIndexes = true,
+    super.cacheExtent,
+    super.dragStartBehavior,
+    super.keyboardDismissBehavior,
+    super.restorationId,
+    super.clipBehavior,
+    super.hitTestBehavior,
+    super.addAutomaticKeepAlives = false,
+  });
+}
+
+class CountConfiguration extends BaseGridConfiguration {
+  CountConfiguration({
+    required this.crossAxisCount,
+    this.mainAxisSpacing = 0.0,
+    this.childAspectRatio = 1.0,
+    this.crossAxisSpacing = 0.0,
+    super.key,
+    super.scrollDirection,
+    super.reverse,
+    super.controller,
+    super.primary,
+    super.physics,
+    super.shrinkWrap,
+    super.padding,
+    super.findChildIndexCallback,
+    super.addRepaintBoundaries = true,
+    super.addSemanticIndexes = true,
+    super.cacheExtent,
+    super.dragStartBehavior,
+    super.keyboardDismissBehavior,
+    super.restorationId,
+    super.clipBehavior,
+    super.hitTestBehavior,
+    super.addAutomaticKeepAlives = false,
+  }) : super(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: crossAxisCount,
+            mainAxisSpacing: mainAxisSpacing,
+            crossAxisSpacing: crossAxisSpacing,
+            childAspectRatio: childAspectRatio,
+          ),
+        );
+
+  final int crossAxisCount;
+  final double mainAxisSpacing;
+  final double childAspectRatio;
+  final double crossAxisSpacing;
+}
+
+class ExtentConfiguration extends BaseGridConfiguration {
+  ExtentConfiguration({
+    required this.maxCrossAxisExtent,
+    this.mainAxisSpacing = 0.0,
+    this.childAspectRatio = 1.0,
+    this.crossAxisSpacing = 0.0,
+    super.key,
+    super.scrollDirection,
+    super.reverse,
+    super.controller,
+    super.primary,
+    super.physics,
+    super.shrinkWrap,
+    super.padding,
+    super.findChildIndexCallback,
+    super.addRepaintBoundaries = true,
+    super.addSemanticIndexes = true,
+    super.cacheExtent,
+    super.dragStartBehavior,
+    super.keyboardDismissBehavior,
+    super.restorationId,
+    super.clipBehavior,
+    super.hitTestBehavior,
+    super.addAutomaticKeepAlives = false,
+  }) : super(
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: maxCrossAxisExtent,
+            mainAxisSpacing: mainAxisSpacing,
+            crossAxisSpacing: crossAxisSpacing,
+            childAspectRatio: childAspectRatio,
+          ),
+        );
+
+  final double maxCrossAxisExtent;
+  final double mainAxisSpacing;
+  final double childAspectRatio;
+  final double crossAxisSpacing;
 }
