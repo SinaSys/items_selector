f5a8f6f01c649ad2a5a43a7a42eca3e8589d6051
SINASYS <sinasystem47@gmail.com>
2025-03-29 18:57:48 +0430

    Change default value of allowUnselectInitialItems to false in all options

--- lib/src/model/select_options.dart
+++ lib/src/model/select_options.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/foundation.dart' show immutable;
 
 @immutable
-abstract class SelectOptions {
+sealed class SelectOptions {
   final bool? allowUnselectInitialItems;
   final int? maxItems;
 
@@ -17,14 +17,14 @@ final class SingleSelectOptions extends SelectOptions {
 
   const SingleSelectOptions({
     this.allowUnselectMainItems = false,
-    super.allowUnselectInitialItems = true,
+    super.allowUnselectInitialItems,
   });
 }
 
 @immutable
 final class MultiSelectOptions extends SelectOptions {
   const MultiSelectOptions({
-    super.allowUnselectInitialItems = false,
+    super.allowUnselectInitialItems,
     super.maxItems,
   });
 }
