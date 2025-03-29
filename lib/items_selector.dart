b1d1e2cae20397cd275cbe6fd9494e10386f3d48
SINASYS <sinasystem47@gmail.com>
2025-03-29 19:03:35 +0430

    Hide state classes in library

--- lib/items_selector.dart
+++ lib/items_selector.dart
@@ -2,13 +2,10 @@ library;
 
 export 'src/model/item_selector.dart';
 export 'src/model/select_options.dart';
-export 'src/model/grid_configuration.dart';
 export 'src/model/wrap_configuration.dart';
 export 'src/model/list_configuration.dart';
+export 'src/model/grid_configuration.dart' hide BaseGridConfiguration;
 
-export 'src/widget/multi_grid_item_selector.dart';
-export 'src/widget/multi_list_item_selector.dart';
-export 'src/widget/single_grid_item_selector.dart';
-export 'src/widget/single_list_item_selector.dart';
-export 'src/widget/single_wrap_item_selector.dart';
-export 'src/widget/multi_wrap_item_selector.dart';
+export 'src/widget/grid_selector.dart' hide GridSelectorState;
+export 'src/widget/list_selector.dart' hide ListSelectorState;
+export 'src/widget/wrap_selector.dart' hide WrapSelectorState;
