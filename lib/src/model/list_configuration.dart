0d04e6ce8ce5c698835aaa331ffb5a729db9241f
SINASYS <sinasystem47@gmail.com>
2025-03-30 00:43:37 +0430

    Add FlexConfiguration class for using row and colum

--- lib/src/model/list_configuration.dart
+++ lib/src/model/list_configuration.dart
@@ -1,22 +1,66 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class ListConfiguration extends ListView {
+sealed class BaseListConfiguration {}
+
+class ListConfiguration extends BaseListConfiguration {
   ListConfiguration({
-    super.key,
-    super.addAutomaticKeepAlives,
-    super.addRepaintBoundaries,
-    super.addSemanticIndexes,
-    super.cacheExtent,
-    super.clipBehavior,
-    super.controller,
-    super.dragStartBehavior,
-    super.hitTestBehavior,
-    super.padding,
-    super.physics,
-    super.primary,
-    super.keyboardDismissBehavior,
-    super.restorationId,
-    super.reverse,
-    super.shrinkWrap,
+    this.scrollDirection,
+    this.scrollBehavior,
+    this.center,
+    this.anchor,
+    this.semanticChildCount,
+    this.cacheExtent,
+    this.clipBehavior,
+    this.controller,
+    this.dragStartBehavior,
+    this.hitTestBehavior,
+    this.padding,
+    this.physics,
+    this.primary,
+    this.keyboardDismissBehavior,
+    this.restorationId,
+    this.reverse,
+    this.shrinkWrap,
   });
+
+  final Axis? scrollDirection;
+  final ScrollViewKeyboardDismissBehavior? keyboardDismissBehavior;
+  final bool? reverse;
+  final ScrollController? controller;
+  final bool? primary;
+  final ScrollPhysics? physics;
+  final ScrollBehavior? scrollBehavior;
+  final bool? shrinkWrap;
+  final Key? center;
+  final double? anchor;
+  final double? cacheExtent;
+  final int? semanticChildCount;
+  final DragStartBehavior? dragStartBehavior;
+  final String? restorationId;
+  final Clip? clipBehavior;
+  final HitTestBehavior? hitTestBehavior;
+  final EdgeInsetsGeometry? padding;
+}
+
+class FlexConfiguration extends BaseListConfiguration {
+  FlexConfiguration({
+    this.mainAxisAlignment = MainAxisAlignment.start,
+    this.mainAxisSize = MainAxisSize.max,
+    this.crossAxisAlignment = CrossAxisAlignment.center,
+    this.textDirection,
+    this.verticalDirection = VerticalDirection.down,
+    this.textBaseline,
+    this.clipBehavior = Clip.none,
+    this.spacing = 0.0,
+  });
+
+  final MainAxisAlignment mainAxisAlignment;
+  final MainAxisSize mainAxisSize;
+  final CrossAxisAlignment crossAxisAlignment;
+  final TextDirection? textDirection;
+  final VerticalDirection verticalDirection;
+  final TextBaseline? textBaseline;
+  final Clip clipBehavior;
+  final double spacing;
 }
