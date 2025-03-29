aaec7062d2373922bdfe766afdbd8777a79e8b33
SINASYS <sinasystem47@gmail.com>
2025-03-30 00:45:34 +0430

    Make Generative constructor of ListSelector accessible for using Row/Column layouts

--- lib/src/widget/list_selector.dart
+++ lib/src/widget/list_selector.dart
@@ -3,24 +3,34 @@ import 'package:flutter/material.dart';
 import 'package:items_selector/items_selector.dart';
 import 'package:items_selector/src/utils/extensions.dart';
 import 'package:items_selector/src/model/base_selector.dart';
+import 'package:items_selector/src/model/list_configuration.dart';
+
+enum ListSelectorType {
+  flex,
+  builder,
+  separated,
+}
 
 class ListSelector<T> extends BaseSelector<T> {
-  final ListConfiguration? listConfiguration;
+  final BaseListConfiguration? configuration;
   final IndexedWidgetBuilder? separatorBuilder;
   final Axis direction;
+  final ListSelectorType selectorType;
 
-  ListSelector._({
+  ListSelector({
     super.key,
     required super.items,
-    required super.mode,
     required super.selectedItems,
     required super.builder,
     super.hasLongPress,
     super.initialItems,
     super.options,
     this.direction = Axis.horizontal,
-  })  : listConfiguration = null,
-        separatorBuilder = null;
+    FlexConfiguration? flexConfiguration,
+  })  : configuration = flexConfiguration,
+        separatorBuilder = null,
+        selectorType = ListSelectorType.flex,
+        super(mode: BaseSelector.determineWidgetType(options));
 
   ListSelector.builder({
     super.key,
@@ -30,9 +40,11 @@ class ListSelector<T> extends BaseSelector<T> {
     super.hasLongPress,
     super.initialItems,
     super.options,
-    this.listConfiguration,
+    ListConfiguration? listConfiguration,
     this.direction = Axis.vertical,
-  })  : separatorBuilder = null,
+  })  : configuration = listConfiguration,
+        separatorBuilder = null,
+        selectorType = ListSelectorType.builder,
         super(mode: BaseSelector.determineWidgetType(options));
 
   ListSelector.separated({
@@ -44,9 +56,11 @@ class ListSelector<T> extends BaseSelector<T> {
     super.hasLongPress,
     super.initialItems,
     super.options,
-    this.listConfiguration,
+    ListConfiguration? listConfiguration,
     this.direction = Axis.vertical,
-  }) : super(
+  })  : configuration = listConfiguration,
+        selectorType = ListSelectorType.separated,
+        super(
           mode: BaseSelector.determineWidgetType(options),
         );
 
@@ -65,11 +79,41 @@ class ListSelectorState<T> extends BaseSelectorState<T> {
       },
     );
 
-    return _buildConfiguredList(items);
+    return switch (listSelectorWidget.selectorType) {
+      ListSelectorType.flex => _buildFlexList(items),
+      _ => _buildConfiguredList(items)
+    };
+  }
+
+  Widget _buildFlexList(List<Widget> items) {
+    final FlexConfiguration? config = listSelectorWidget.configuration as FlexConfiguration?;
+
+    return switch (listSelectorWidget.direction) {
+      Axis.horizontal => Row(
+          mainAxisAlignment: config?.mainAxisAlignment ?? MainAxisAlignment.start,
+          spacing: config?.spacing ?? 0.0,
+          textDirection: config?.textDirection,
+          verticalDirection: config?.verticalDirection ?? VerticalDirection.down,
+          crossAxisAlignment: config?.crossAxisAlignment ?? CrossAxisAlignment.center,
+          mainAxisSize: config?.mainAxisSize ?? MainAxisSize.max,
+          textBaseline: config?.textBaseline,
+          children: items,
+        ),
+      Axis.vertical => Column(
+          mainAxisAlignment: config?.mainAxisAlignment ?? MainAxisAlignment.start,
+          spacing: config?.spacing ?? 0.0,
+          textDirection: config?.textDirection,
+          verticalDirection: config?.verticalDirection ?? VerticalDirection.down,
+          crossAxisAlignment: config?.crossAxisAlignment ?? CrossAxisAlignment.center,
+          mainAxisSize: config?.mainAxisSize ?? MainAxisSize.max,
+          textBaseline: config?.textBaseline,
+          children: items,
+        )
+    };
   }
 
   Widget _buildConfiguredList(List<Widget> items) {
-    final ListConfiguration? config = listSelectorWidget.listConfiguration;
+    final ListConfiguration? config = listSelectorWidget.configuration as ListConfiguration?;
 
     if (listSelectorWidget.separatorBuilder != null) {
       return ListView.separated(
@@ -101,14 +145,11 @@ class ListSelectorState<T> extends BaseSelectorState<T> {
       reverse: config?.reverse ?? false,
       clipBehavior: config?.clipBehavior ?? Clip.hardEdge,
       controller: config?.controller,
-      itemExtent: config?.itemExtent,
       primary: config?.primary,
       physics: config?.physics,
       cacheExtent: config?.cacheExtent,
-      prototypeItem: config?.prototypeItem,
       restorationId: config?.restorationId,
       shrinkWrap: config?.shrinkWrap ?? false,
-      itemExtentBuilder: config?.itemExtentBuilder,
       keyboardDismissBehavior: config?.keyboardDismissBehavior ?? ScrollViewKeyboardDismissBehavior.manual,
       dragStartBehavior: config?.dragStartBehavior ?? DragStartBehavior.start,
       hitTestBehavior: config?.hitTestBehavior ?? HitTestBehavior.opaque,
