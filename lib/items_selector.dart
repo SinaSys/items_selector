2fe6d8643062a54897660b7be83373f8c6633428
SINASYS <sinasystem47@gmail.com>
2025-03-30 00:46:11 +0430

    hide BaseLitConfiguration in library

--- lib/items_selector.dart
+++ lib/items_selector.dart
@@ -3,9 +3,9 @@ library;
 export 'src/model/item_selector.dart';
 export 'src/model/select_options.dart';
 export 'src/model/wrap_configuration.dart';
-export 'src/model/list_configuration.dart';
+export 'src/model/list_configuration.dart' hide BaseListConfiguration;
 export 'src/model/grid_configuration.dart' hide BaseGridConfiguration;
 
 export 'src/widget/grid_selector.dart' hide GridSelectorState;
-export 'src/widget/list_selector.dart' hide ListSelectorState;
+export 'src/widget/list_selector.dart' hide ListSelectorState, ListSelectorType;
 export 'src/widget/wrap_selector.dart' hide WrapSelectorState;
