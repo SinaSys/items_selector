9b69174485e3f3c9640c583d93aa2e0e1597250d
SINASYS <sinasystem47@gmail.com>
2025-04-04 22:00:14 +0430

    Define typedef for RadioSelector

--- lib/src/utils/typedefs.dart
+++ lib/src/utils/typedefs.dart
@@ -2,4 +2,5 @@ import 'package:flutter/widgets.dart' show BuildContext;
 import 'package:items_selector/src/model/item_selector.dart';
 
 typedef OnSelectedItemsChanged<T> = void Function(List<T>, int index);
+typedef OnSelectedRadioChanged<T> = void Function(T);
 typedef ItemBuilder = ItemSelector Function(BuildContext context, int index);
