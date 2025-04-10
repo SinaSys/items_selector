5a7a5ede121d59955c8a9f670bcecc58eca0b6b3
SINASYS <sinasystem47@gmail.com>
2025-04-10 19:58:07 +0430

    Add new type definition for RadioSelector

--- lib/src/utils/typedefs.dart
+++ lib/src/utils/typedefs.dart
@@ -1,6 +1,7 @@
+import 'package:items_selector/items_selector.dart';
 import 'package:flutter/widgets.dart' show BuildContext;
-import 'package:items_selector/src/model/item_selector.dart';
 
 typedef OnSelectedItemsChanged<T> = void Function(List<T>, int index);
-typedef OnSelectedRadioChanged<T> = void Function(T);
+typedef OnSelectedRadioChanged<T> = void Function(T,int index);
+typedef OnSelectedCheckboxChanged = void Function(List<CheckBoxSelectorItem>, List<int> index);
 typedef ItemBuilder = ItemSelector Function(BuildContext context, int index);
