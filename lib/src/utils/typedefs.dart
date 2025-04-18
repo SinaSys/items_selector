8d66dd471406a4495f206fb5fda6dee5cf5430e2
SINASYS <sinasystem47@gmail.com>
2025-04-18 19:05:13 +0430

    Limit OnSelectedRadioChanged typedef to RadioSelectorItemBase subtypes

--- lib/src/utils/typedefs.dart
+++ lib/src/utils/typedefs.dart
@@ -2,6 +2,6 @@ import 'package:items_selector/items_selector.dart';
 import 'package:flutter/widgets.dart' show BuildContext;
 
 typedef OnSelectedItemsChanged<T> = void Function(List<T>, int index);
-typedef OnSelectedRadioChanged<T> = void Function(T,int index);
+typedef OnSelectedRadioChanged<T extends RadioSelectorItemBase> = void Function(T, int index);
 typedef OnSelectedCheckboxChanged = void Function(List<CheckBoxSelectorItem>, List<int> index);
 typedef ItemBuilder = ItemSelector Function(BuildContext context, int index);
