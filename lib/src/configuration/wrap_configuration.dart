c6fba665e1fc7758073034323ab65908b2621973
SINASYS <sinasystem47@gmail.com>
2025-04-18 21:17:55 +0430

    Rewrite WrapConfiguration with WrapAttributes interface

--- lib/src/configuration/wrap_configuration.dart
+++ lib/src/configuration/wrap_configuration.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/widgets.dart';
+import 'package:items_selector/src/common/wrap_attributes.dart';
 
-class WrapConfiguration extends Wrap {
+class WrapConfiguration implements WrapAttributes {
   WrapConfiguration({
-    super.key,
-    super.alignment,
-    super.clipBehavior,
-    super.crossAxisAlignment,
-    super.direction,
-    super.runAlignment,
-    super.runSpacing,
-    super.spacing,
-    super.textDirection,
-    super.verticalDirection,
-  }) : super(children: []);
+    this.direction = Axis.horizontal,
+    this.alignment = WrapAlignment.start,
+    this.spacing = 0.0,
+    this.runAlignment = WrapAlignment.start,
+    this.runSpacing = 0.0,
+    this.wrapCrossAxisAlignment = WrapCrossAlignment.start,
+    this.textDirection,
+    this.verticalDirection = VerticalDirection.down,
+    this.clipBehavior = Clip.none,
+  });
+
+  @override
+  final Axis direction;
+  @override
+  final WrapAlignment alignment;
+  @override
+  final double spacing;
+  @override
+  final WrapAlignment runAlignment;
+  @override
+  final double runSpacing;
+  @override
+  final WrapCrossAlignment wrapCrossAxisAlignment;
+  @override
+  final TextDirection? textDirection;
+  @override
+  final VerticalDirection verticalDirection;
+  @override
+  final Clip clipBehavior;
 }
